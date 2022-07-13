bba219d8edc2fecba551152317d2284a60e3db2d
Minus <minus.official.01@gmail.com>
2022-07-13 19:11:40 +0530

    Completed adding a rough structure of MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.port || 5000;
+
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() =>
+      app.listen(PORT, () => console.log(`Server running at ${PORT}`)),
+    )
+    .catch((error) => console.log(error.message));
+};
+
+connectDB();
